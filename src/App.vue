58f03443d05647445f7380232ed4c8100e242c12
zequanwu <zequanwu@xiaoe-tech.com>
2022-09-19 15:38:12 +0800

    feat: 新增一个节点

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
+<!--
+ * @Author: zequan.wu
+ * @Date: 2022-09-19 15:28:14
+ * @LastEditors: zequan.wu
+ * @LastEditTime: 2022-09-19 15:37:45
+ * @FilePath: \git-changelog\src\App.vue
+ * @Description: 
+ * 
+-->
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>改一下</div>
   </div>
 </template>
 
